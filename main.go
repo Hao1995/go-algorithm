a61ec3e4a605d59195fa227985766708b477a717
Harry Lu <harry.nan.825@gmail.com>
2023-12-17 14:53:30 +0800

    main.go hello world

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	idxMap := make(map[rune]int)
-	idxMap['a'] = 1
-
-	s := "abcde"
-	fmt.Println(s[0:])
-	fmt.Println(s[4:5])
-
-	// print byte
-	fmt.Println(s[0])
-	// print rune
-	fmt.Println(rune(s[0]))
+	fmt.Println("Hello, world.")
 }
