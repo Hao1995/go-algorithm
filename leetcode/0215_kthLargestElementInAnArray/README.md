cbab3b049e310b3dba9b9eb379990d21bce70d71
Harry Lu <harry.nan.825@gmail.com>
2024-05-06 09:58:17 +0800

    215 update time complexity using heap.Init

--- leetcode/0215_kthLargestElementInAnArray/README.md
+++ leetcode/0215_kthLargestElementInAnArray/README.md
@@ -5,7 +5,7 @@ MaxHeap(Priority Queue)
 Even the question requirement is without sorting, I can push the num to a priority queue to achieve the same functionality.
 
 ## Approach
-1. Store nums to a priority queue. Time complexity is O(n*logn)
+1. Init nums to a priority queue. Time complexity is O(n)
 2. Retrieve top k elements from the priority queue. Time complexity is O(k*logn)
 
-Total time complexity is O((k+n)logn).
+Total time complexity is O(n+klogn).
--- leetcode/0215_kthLargestElementInAnArray/kthLargestElementInAnArray.go
+++ leetcode/0215_kthLargestElementInAnArray/kthLargestElementInAnArray.go
@@ -2,33 +2,29 @@ package kthlargestelementinanarray
 
 import "container/heap"
 
-type MaxHeap struct {
-	data []int
-}
+type MaxHeap []int
 
-func (this MaxHeap) Len() int           { return len(this.data) }
-func (this MaxHeap) Less(i, j int) bool { return this.data[i] > this.data[j] }
-func (this MaxHeap) Swap(i, j int)      { this.data[i], this.data[j] = this.data[j], this.data[i] }
-func (this *MaxHeap) Push(item any)     { this.data = append(this.data, item.(int)) }
+func (this MaxHeap) Len() int           { return len(this) }
+func (this MaxHeap) Less(i, j int) bool { return this[i] > this[j] }
+func (this MaxHeap) Swap(i, j int)      { this[i], this[j] = this[j], this[i] }
+func (this *MaxHeap) Push(item any)     { *this = append(*this, item.(int)) }
 func (this *MaxHeap) Pop() any {
+	old := *this
 	var item int
-	item, this.data = this.data[len(this.data)-1], this.data[:len(this.data)-1]
+	item, old = old[len(old)-1], old[:len(old)-1]
+	*this = old
 	return item
 }
 
 func findKthLargest(nums []int, k int) int {
-	pq := &MaxHeap{}
-	heap.Init(pq)
-
-	// O(n*logn)
-	for _, num := range nums {
-		heap.Push(pq, num)
-	}
+	// O(n): https://pkg.go.dev/container/heap#Init
+	pq := MaxHeap(nums)
+	heap.Init(&pq)
 
 	// O(k*logn)
 	var ans int
 	for i := 0; i < k; i++ {
-		ans = heap.Pop(pq).(int)
+		ans = heap.Pop(&pq).(int)
 	}
 
 	return ans
