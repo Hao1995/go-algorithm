429815d04233af509a15fca5aaa7971f9810b902
Harry Lu <harry.nan.825@gmail.com>
2024-09-16 10:38:00 +0800

    100. same tree

---
+++ leetcode/0100_sameTree/README.md
@@ -0,0 +1,7 @@
+# 100. Same Tree
+https://leetcode.com/problems/same-tree/
+
+## Intuition
+Use DFS to compare each node of `p` and `q` trees.
+
+## Approach
---
+++ leetcode/0100_sameTree/sameTree.go
@@ -0,0 +1,23 @@
+package sametree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	return dfs(p, q)
+}
+
+func dfs(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+
+	return dfs(p.Left, q.Left) && dfs(p.Right, q.Right)
+}
