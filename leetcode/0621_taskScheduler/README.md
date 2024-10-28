e380918587407f6cbacb18e07bc9c3742206db9e
Harry Lu <harry.nan.825@gmail.com>
2024-10-28 10:34:13 +0800

    621. Task Scheduler. Update with PQ

--- leetcode/0621_taskScheduler/README.md
+++ leetcode/0621_taskScheduler/README.md
@@ -2,14 +2,6 @@
 
 ## Intuition
 Priority Queue.
-If `n` is greater than 0, we can't execute the same task at the next time.
-So, in order to complete all the tasks with the minimum execution time, we need to execute the task with the maximum number.
-
-I have the above intuition, but I can't implement it by myself.
-I found solutions from:
-1. https://www.youtube.com/live/3DZE7cfgYyg?si=M_IpBHgL4QkHi88x
-2. https://leetcode.com/problems/task-scheduler/solutions/3612057/golang-optimal-approach-fully-explained-easy-solution
-
 
 ## Approach
 // [a,a,a,b,b,b], len(pq)=2
@@ -17,3 +9,8 @@ I found solutions from:
 // n=1 -> [a,b],[a,b],[a,b] -> 6 (2+2+2)
 // n=2 -> [a,b,*],[a,b,*],[a,b] -> 8 (3+3+2)
 // n=3 -> [a,b,*,*],[a,b,*,*],[a,b] -> 10 (4+4+2)
+
+// Input length = m
+// Interval = n
+// PQ size = k, k <= m
+// O(k*logk) >> even we have a O(nlogk) in the first nested loop, but we finally iterate all keys in PQ, so the time complexity would be O(klogk)
--- leetcode/0621_taskScheduler/taskScheduler.go
+++ leetcode/0621_taskScheduler/taskScheduler.go
@@ -1,52 +1,62 @@
 package taskscheduler
 
-import "sort"
+import "container/heap"
+
+type LetterCount struct {
+	Letter byte
+	Count  int
+}
+
+type MaxHeap []LetterCount
+
+func (this MaxHeap) Len() int           { return len(this) }
+func (this MaxHeap) Less(i, j int) bool { return this[i].Count > this[j].Count }
+func (this MaxHeap) Swap(i, j int)      { this[i], this[j] = this[j], this[i] }
+func (this *MaxHeap) Push(item any)     { *this = append(*this, item.(LetterCount)) }
+func (this *MaxHeap) Pop() any {
+	item := (*this)[this.Len()-1]
+	*this = (*this)[:this.Len()-1]
+	return item
+}
 
 func leastInterval(tasks []byte, n int) int {
-	// O(m)
 	countMap := make(map[byte]int)
-	for _, c := range tasks {
-		countMap[c] += 1
+	for _, t := range tasks {
+		countMap[t]++
 	}
 
-	// O(m)
-	pq := make([]int, len(countMap))
-	var i int
-	for _, count := range countMap {
-		pq[i] = count
-		i++
+	pq := MaxHeap(make([]LetterCount, 0, len(countMap)))
+	heap.Init(&pq)
+	for c, count := range countMap {
+		heap.Push(&pq, LetterCount{
+			Letter: c,
+			Count:  count,
+		})
 	}
-
-	// O(mlonm)
-	sort.Slice(pq, func(i, j int) bool { return pq[i] > pq[j] })
 
 	var ans int
-	for len(pq) > 0 {
-		var tmpPQ []int
-		for i := 0; i <= n; i++ {
-			if len(pq) > 0 {
-				var p int
-				p, pq = pq[0], pq[1:]
-				tmpPQ = append(tmpPQ, p-1)
+	for pq.Len() > 0 {
+		var tmpPQ MaxHeap
+
+		var count int
+		slots := n + 1
+		for i := 0; i < slots && pq.Len() > 0; i++ {
+			item := heap.Pop(&pq).(LetterCount)
+			item.Count--
+			if item.Count > 0 {
+				tmpPQ = append(tmpPQ, item)
 			}
-			ans++
-			if len(pq) == 0 {
-				break
-			}
+			count++
+		}
+		ans += count
+
+		// idle: add the remaining number
+		if tmpPQ.Len() > 0 {
+			ans += slots - count
 		}
 
-		for _, v := range tmpPQ {
-			if v > 0 {
-				pq = append(pq, v)
-			}
-		}
-		sort.Slice(pq, func(i, j int) bool { return pq[i] > pq[j] })
-
-		// If n == len(unique(tasks)), there must be idle time. So we need to add it.
-		// If len(pq) == 0, it means there is no idle time that needs to be added.
-		// EX: tasks=["A","A","A","B","B","B"], n=2, `A -> B -> idle`
-		if len(pq) > 0 {
-			ans += n - len(tmpPQ) + 1
+		for _, item := range tmpPQ {
+			heap.Push(&pq, item)
 		}
 	}
 
