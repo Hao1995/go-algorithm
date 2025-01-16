598c0075e08c6be8c536a4589bc452e4646e857e
Harry Lu <harry.nan.825@gmail.com>
2025-01-16 11:21:39 +0800

    90. explains subsets

--- leetcode/0090_subsetsII/README.md
+++ leetcode/0090_subsetsII/README.md
@@ -21,3 +21,6 @@ DFS to explore any possibilities.
 //              is empty, ans=[[1,2,2], [1,2], [1], [2,2], [2]]
 //      2, skip all 2, []
 //          is empty, ans=[[1,2,2], [1,2], [1], [2,2], [2], []]
+
+Time complexity: O(n*2^n)
+Space complexity: O(n*2^n) // we need to return all subsets
--- leetcode/0090_subsetsII/subsetsII.go
+++ leetcode/0090_subsetsII/subsetsII.go
@@ -4,10 +4,15 @@ import "sort"
 
 func subsetsWithDup(nums []int) [][]int {
 	var ans [][]int
+
+	// O(nlogn)
 	sort.Slice(nums, func(i, j int) bool {
 		return nums[i] < nums[j]
 	})
 
+	// include or skip >> 2, n elements >> 2^n, O(2^n)
+	// finally we're going to copy the list to ans >> O(n)
+	// O(n*2^n)
 	var dfs func(list []int, tmpNums []int)
 	dfs = func(list []int, tmpNums []int) {
 		if len(tmpNums) == 0 {
