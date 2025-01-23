61c878eedc995d32561b86cd7698389325b5af14
Harry Lu <harry.nan.825@gmail.com>
2025-01-23 11:05:04 +0800

    48. Rotate Image - V2 - Simplified Version

--- leetcode/0048_rotateImage/rotateImage.go
+++ leetcode/0048_rotateImage/rotateImage.go
@@ -1,28 +1,15 @@
 package rotateimage
 
 func rotate(matrix [][]int) {
-	leftLimit, rightLimit, upperLimit, bottomLimit := 0, len(matrix[0])-1, 0, len(matrix)-1
-	var p1, p2, p3, p4 []int = []int{upperLimit, leftLimit}, []int{upperLimit, rightLimit}, []int{bottomLimit, rightLimit}, []int{bottomLimit, leftLimit}
-	for {
-		matrix[p2[0]][p2[1]], matrix[p3[0]][p3[1]], matrix[p4[0]][p4[1]], matrix[p1[0]][p1[1]] = matrix[p1[0]][p1[1]], matrix[p2[0]][p2[1]], matrix[p3[0]][p3[1]], matrix[p4[0]][p4[1]]
+	var SIZE int = len(matrix)
 
-		p1[1]++
-		p2[0]++
-		p3[1]--
-		p4[0]--
-		if p1[1] == rightLimit {
-			leftLimit++
-			rightLimit--
-			upperLimit++
-			bottomLimit--
-			if leftLimit > rightLimit || upperLimit > bottomLimit {
-				// check if finished?
-				break
-			}
-			p1, p2, p3, p4 = []int{upperLimit, leftLimit}, []int{upperLimit, rightLimit}, []int{bottomLimit, rightLimit}, []int{bottomLimit, leftLimit}
-		} else if p1[1] > rightLimit {
-			// check if finished?
-			break
+	var layer int = 0
+	for layer < SIZE-1-layer {
+		var boundary int = SIZE - 1 - layer
+		for idx := 0; layer+idx < boundary; idx++ {
+			matrix[idx+layer][boundary], matrix[boundary][boundary-idx], matrix[boundary-idx][layer], matrix[layer][layer+idx] =
+				matrix[layer][layer+idx], matrix[layer+idx][boundary], matrix[boundary][boundary-idx], matrix[boundary-idx][layer]
 		}
+		layer++
 	}
 }
